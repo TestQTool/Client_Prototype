56015d54fabb972107976a050858892badc6385e
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-02 16:24:38 +0530

    Generated Rest Assured scripts for selected test cases

---
+++ src/test/java/tests/frontend/ag_helix/general/VerifyThatLoginImplementsHTTPSEncryption_Api674Test.java
@@ -0,0 +1,32 @@
+package tests.frontend.ag_helix.general;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.frontend.ag_helix.QentrixConfig;
+import tests.frontend.ag_helix.QentrixReport;
+import tests.frontend.ag_helix.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatLoginImplementsHTTPSEncryption_Api674Test {
+
+    @Test(description = "Verify that login implements HTTPS encryption")
+    public void verifyThatLoginImplementsHTTPSEncryption() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        Response response = given()
+                .headers(QentrixConfig.authHeaders())
+        .when()
+                .get("/http");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 200, "Unexpected response status code");
+        Assert.assertEquals(String.valueOf(response.jsonPath().get("id")), "", "Unexpected JSON value for id");
+    }
+}
