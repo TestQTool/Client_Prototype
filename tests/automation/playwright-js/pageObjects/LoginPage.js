088bdb751b99f7c6585c71878135ea6cff97a5e7
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-02 09:32:31 +0530

    Generate automation script: tests/automation/playwright-js/pageObjects/LoginPage.js

---
+++ tests/automation/playwright-js/pageObjects/LoginPage.js
@@ -0,0 +1,24 @@
+const { expect } = require('@playwright/test');
+
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.locator('input[name="username"]');
+    this.passwordInput = page.locator('input[name="password"]');
+    this.loginButton = page.locator('button[type="submit"]');
+  }
+
+  async navigateToLoginPage(url) {
+    await this.page.goto(url);
+    await expect(this.page).toHaveURL(/login/);
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+    await this.page.waitForLoadState('networkidle');
+  }
+}
+
+module.exports = LoginPage;
