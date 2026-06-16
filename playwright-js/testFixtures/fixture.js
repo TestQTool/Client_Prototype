7acbef9f03896bb2ad9ec59663c8af450b0feb12
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-16 13:17:56 +0530

    [AUTO] [GEN-1] [WI:764] Generated automation scripts

---
+++ playwright-js/testFixtures/fixture.js
@@ -0,0 +1,10 @@
+import { test as base } from '@playwright/test';
+import LeavePage from '../pages/leavePage.js';
+
+const test = base.extend({
+    leavePage: async ({ page }, use) => {
+        await use(new LeavePage(page));
+    },
+});
+
+export default test;
