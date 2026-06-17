490aba4a3c0bde984f5e66ca86e74783d6c14bf6
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-17 13:08:05 +0530

    [AUTO] [GEN-1] [WI:SCRUM-150] Generated automation scripts

--- playwright-js/testFixtures/fixture.js
+++ playwright-js/testFixtures/fixture.js
@@ -1,14 +1,10 @@
 import { test as base } from '@playwright/test';
-import BasePage from '../pages/basePage.js';
 import RegistrationPage from '../pages/registrationPage.js';
 
 const test = base.extend({
-  basePage: async ({ page }, use) => {
-    await use(new BasePage(page));
-  },
-  registrationPage: async ({ page }, use) => {
-    await use(new RegistrationPage(page));
-  }
+    registrationPage: async ({ page }, use) => {
+        await use(new RegistrationPage(page));
+    }
 });
 
 export default test;
