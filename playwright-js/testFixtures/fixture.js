a90f60569431b998965dcb9094078062b75800c4
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-23 10:18:16 +0530

    [AUTO] [GEN-1] [WI:2] Generated automation scripts

--- playwright-js/testFixtures/fixture.js
+++ playwright-js/testFixtures/fixture.js
@@ -1,11 +1,11 @@
 import { test as base } from '@playwright/test';
-import AuthenticationPage from '../pages/AuthenticationPage.js';
+import LoginPage from '../pages/loginPage.js';
 
 const test = base.extend({
-  authenticationPage: async ({ page }, use) => {
-    await use(new AuthenticationPage(page));
-  },
+    loginPage: async ({ page }, use) => {
+        await use(new LoginPage(page));
+    },
 });
 
 export default test;
-export { expect } from '@playwright/test';
+
