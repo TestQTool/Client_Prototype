e62719487aec627d2434705c90fef21c8ccc17b1
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-24 12:54:00 +0530

    [AUTO] [GEN-1] [WI:1503] Generated automation scripts

---
+++ playwright-js/pageObjects/LoginPage.js
@@ -0,0 +1,9 @@
+// LoginPage locators
+// TODO: verify selectors against live app
+
+export const usernameInput = '#user-name';
+export const passwordInput = '#password';
+export const loginButton = '#login-button';
+export const errorMessage = '[data-test="error"]';
+export const inventoryContainer = '#inventory_container';
+
