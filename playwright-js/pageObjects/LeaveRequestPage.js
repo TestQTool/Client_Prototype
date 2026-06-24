a6ba921af7e29d953b593842e8ba9d495a0540bd
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-06-24 12:54:01 +0530

    [AUTO] [GEN-1] [WI:1503] Generated automation scripts

---
+++ playwright-js/pageObjects/LeaveRequestPage.js
@@ -0,0 +1,14 @@
+// LeaveRequestPage locators
+// TODO: verify selectors against live app
+
+export const leaveTypeDropdown = '#leave-type';
+export const startDateInput = '#start-date';
+export const endDateInput = '#end-date';
+export const reasonInput = '#leave-reason';
+export const submitButton = 'button[type="submit"]';
+export const validationError = '.validation-error';
+export const confirmationMessage = '.confirmation-message';
+export const totalLeaveDaysDisplay = '#total-leave-days';
+export const leaveRequestStatus = '.leave-status';
+export const leaveRequestRow = '.leave-request-row';
+
