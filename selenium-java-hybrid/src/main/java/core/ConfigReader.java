0b40167789dee7ee923ed0f00bc36335b54c1047
Kishore-Azmira <azmira_kishore@quality-matrix.com>
2026-06-26 17:21:50 +0530

    test

--- selenium-java-hybrid/src/main/java/core/ConfigReader.java
+++
@@ -1,9 +0,0 @@
-package core;
-
-public final class ConfigReader {
-    private ConfigReader() {}
-
-    public static String get(String key, String defaultValue) {
-        return utils.ConfigReader.get(key, defaultValue);
-    }
-}
--- selenium-java-hybrid/src/test/java/HealthTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
-class HealthTest {
-    @Test
-    void frameworkCompiles() {
-        assertThat(true).isTrue();
-    }
-}
