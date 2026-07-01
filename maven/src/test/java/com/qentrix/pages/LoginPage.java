bed93a2969469de86ff78df40724243cdc148054
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-01 05:50:04 -0500

    Merge automation scripts for 1732
    [AUTO] [GEN-1] [WI:1732] Generated automation scripts

---
+++ maven/src/test/java/com/qentrix/pages/LoginPage.java
@@ -0,0 +1,70 @@
+package com.qentrix.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//a[contains(text(),'Forgot Password') or contains(@class,'forgot-password')]")
+    private WebElement forgotPasswordLink;
+
+    @FindBy(xpath = "//div[@class='error-message-container']")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(String username) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public boolean isForgotPasswordLinkDisplayed() {
+        try {
+            return forgotPasswordLink.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void clickForgotPasswordLink() {
+        forgotPasswordLink.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public boolean isLoginPageDisplayed() {
+        return usernameField.isDisplayed() && passwordField.isDisplayed();
+    }
+}
