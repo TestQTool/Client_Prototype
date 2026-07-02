56a96f79e62c0084b29e48ae5b4e972822622e1b
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-02 04:51:37 -0500

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/java/tests/pagination/VerifyThatAPIReturnsPaginatedResultsFeedbackList_Api1327Test.java
@@ -0,0 +1,36 @@
+package tests.pagination;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatAPIReturnsPaginatedResultsFeedbackList_Api1327Test {
+
+    @Test(description = "Verify that API returns paginated results when feedback list exceeds limit")
+    public void verifyThatAPIReturnsPaginatedResultsWhenFeedbackListExceedsLimit() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .get("/user");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 200, "Unexpected response status code");
+        Assert.assertFalse(response.getBody().asString().isBlank(), "Response body should not be empty");
+    }
+}
