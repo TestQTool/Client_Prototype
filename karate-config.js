66cf894357426e2dbeff32db30bbf9d86fd7bb0a
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-02 07:57:17 -0500

    Generated API Test Automation scripts for selected test cases

---
+++ karate-config.js
@@ -0,0 +1,9 @@
+function fn() {
+  var config = {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    authType: 'JWT',
+    authHeaders: {'Authorization': 'Bearer jwt'},
+    authQueryParams: {}
+  };
+  return config;
+}
