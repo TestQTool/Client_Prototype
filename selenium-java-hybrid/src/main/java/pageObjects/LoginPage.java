ebecc707191beb626a34d96e8108f87d79d5d2ce
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-08 07:56:11 -0500

    [AUTO] [GEN-1] [WI:2183] Generated automation scripts

---
+++ selenium-java-hybrid/src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,50 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+/**
+ * Page Object for Login Page
+ * Project: 360GlobalNet
+ */
+public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    // Locators
+    private By usernameField = By.name("username");
+    private By passwordField = By.name("password");
+    private By loginButton = By.xpath("//button[@type='submit']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void enterUsername(String username) {
+        WebElement usernameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField));
+        usernameElement.clear();
+        usernameElement.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordElement = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField));
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(loginButton));
+        loginBtn.click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLoginButton();
+    }
+}
