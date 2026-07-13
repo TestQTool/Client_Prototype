f1c539cf81b124ce0dcc30f82d60fc5e66228a00
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-13 02:46:45 -0500

    [AUTO] [GEN-1] [WI:1800] Generated automation scripts

---
+++ selenium-java-bdd/src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,51 @@
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Hooks {
+    private static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        String browser = System.getProperty("browser", "chrome");
+        
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "chrome":
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(options);
+                break;
+        }
+        
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
