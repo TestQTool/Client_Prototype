4fbd71f4076b9f6771c46ba7239db0de4ad11201
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-13 05:14:46 -0500

    [AUTO] [GEN-1] [WI:1802] Generated automation scripts

---
+++ playwright-js/pageObjects/LoginPage.js
@@ -0,0 +1,17 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = 'input[name="username"]';
+    this.passwordInput = 'input[name="password"]';
+    this.loginButton = 'button[type="submit"]';
+  }
+
+  async login(username, password) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.loginButton);
+    await this.page.waitForLoadState('networkidle');
+  }
+}
+
+module.exports = LoginPage;
