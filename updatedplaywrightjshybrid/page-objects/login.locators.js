84939a1cdb68d2a734597e59e363b902022d333a
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-15 16:10:01 +0530

    [AUTO] [GEN-1] [WI:2343] Generated automation scripts

---
+++ updatedplaywrightjshybrid/page-objects/login.locators.js
@@ -0,0 +1,9 @@
+// TODO: Verify selectors against actual application DOM
+export const LoginLocators = {
+  usernameField: 'input[name="username"]',
+  passwordField: 'input[name="password"]',
+  loginButton: 'input[type="submit"][value="Log In"]',
+  errorMessage: '.error',
+  loginPanel: '#loginPanel'
+};
+
