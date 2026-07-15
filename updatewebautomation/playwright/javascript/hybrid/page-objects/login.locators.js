f322b4b6d8d448a3b355c9c5c20728a83d640757
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-15 12:46:16 +0530

    [AUTO] [GEN-1] [WI:2337] Generated automation scripts

---
+++ updatewebautomation/playwright/javascript/hybrid/page-objects/login.locators.js
@@ -0,0 +1,14 @@
+/**
+ * Login Page Locators
+ * TestCase ID: 2337
+ * TODO: Verify selectors against actual application DOM
+ */
+
+module.exports = {
+  usernameInput: 'input[name="username"]',
+  passwordInput: 'input[name="password"]',
+  loginButton: 'input[type="submit"][value="Log In"]',
+  accountOverviewHeading: 'h1:has-text("Accounts Overview")',
+  accountTable: '#accountTable'
+};
+
