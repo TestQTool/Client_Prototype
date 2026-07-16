633fd7f9ba5304f37fede8aa9b757b5344ab52e4
Quality Matrix <163138544+TestQTool@users.noreply.github.com>
2026-07-16 18:03:35 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/resources/karate-config.js
@@ -0,0 +1,13 @@
+function fn() {
+  var config = {
+    baseUrl: 'https://www.amazon.in/',
+    authType: 'none',
+    token: '',
+    username: '',
+    password: '',
+    apiKeyName: '',
+    apiKeyValue: '',
+    apiKeyLocation: ''
+  };
+  return config;
+}
